c6cee3a416e65f1d766b12c67360f9123bb24138
David Fuentes <davidf0827@gmail.com>
2020-08-07 19:06:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    num_of_sentences = []
+    
+    num_of_sentences << self.split(/["!", "?", "."]/)
+    num_of_sentences.reject{|e| e.length == 0}
+    num_of_sentences.count
+    binding.pry
+    true
   end
 end
